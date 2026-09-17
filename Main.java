e42a689ab9572b8d11126ab65ce1efda4b55cb51
KevinW2027 <kevin.wang2027@gbwl.org>
2026-09-17 12:56:42 +0000

    commit

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 /*
 This is my comment space
+
+Algorithm: step-by-step process to accomplish a task
+Pseudocode: simplified version of code used to design a program
+Sequencing: Creating the order of steps for an algorithm
+Java Program Structure: Class -> Contain Objects
+public class Sandwich{ -> blueprint
+   public static void main() <--entry point into our program
+
+                     }
+
+Compile -> turns java into computer code which is a class file
 */
 
 public class Main {
